5643eb20f0d35a0d8efa324b0f6a3ac2f20717de
lucyhartigan <lmh8583@nyu.edu>
2025-12-09 23:03:51 -0500

    resolve merge conflict

--- ml-recommender/service/external/spoonacular_api.py
+++ ml-recommender/service/external/spoonacular_api.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 import requests
 import os
 
-API_KEY = os.getenv("SPOONACULAR_API_KEY", "3a09c46bac16494eabe50322b303b9a9")
+API_KEY = os.getenv("SPOONACULAR_API_KEY", "9de17c6ff9a74129b43ad731e1b054e4")
 if not API_KEY:
     raise ValueError("SPOONACULAR_API_KEY is not set in environment variables")
 
